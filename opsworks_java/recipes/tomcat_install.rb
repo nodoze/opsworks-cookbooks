21710fe87afd7c692c5bf706bb98d0eb536ba3df
oeberle <oeberle@cellmobs.com>
2014-12-22 17:59:22 -0800

    tomcat recipe fix

--- opsworks_java/recipes/tomcat_install.rb
+++ opsworks_java/recipes/tomcat_install.rb
@@ -5,8 +5,12 @@ tomcat_pkgs = value_for_platform_family(
 
 if node['opsworks_java']['tomcat']['deploy_manager_apps']
   tomcat_pkgs << value_for_platform(
-    'debian' => ["tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin"],
-    'rhel' => ["tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin-webapps"]
+   %w{ debian ubuntu } => {
+    'default' => "tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin",
+  }, 
+  %w{ centos redhat fedora amazon scientific oracle } => {
+    'default' => "tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin-webapps",
+  }
   )
 end
 
