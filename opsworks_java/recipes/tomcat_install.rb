4e69a75b3de0d04fc12440786f87dd40485e6f34
oeberle <oeberle@cellmobs.com>
2014-12-22 17:36:40 -0800

    testing manager install

--- opsworks_java/recipes/tomcat_install.rb
+++ opsworks_java/recipes/tomcat_install.rb
@@ -3,6 +3,31 @@ tomcat_pkgs = value_for_platform_family(
   'rhel' => ["tomcat#{node['opsworks_java']['tomcat']['base_version']}", 'tomcat-native']
 )
 
+if node['opsworks_java']['tomcat']['deploy_manager_apps']
+  tomcat_pkgs << value_for_platform(
+    'debian' => ["tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin"],
+    'rhel' => ["tomcat#{node['opsworks_java']['tomcat']['base_version']}-admin-webapps"],
+  }
+  )
+end
+
+unless node['opsworks_java']['tomcat']['deploy_manager_apps']
+  directory "#{node['opsworks_java']['tomcat']['webapps_base_dir']}/manager" do
+    action :delete
+    recursive true
+  end
+  file "#{node['opsworks_java']['tomcat']['catalina_base_dir']}/Catalina/localhost/manager.xml" do
+   action :delete
+  end
+  directory "#{node['opsworks_java']['tomcat']['webapps_base_dir']}/host-manager" do
+    action :delete
+    recursive true
+  end
+  file "#{node['opsworks_java']['tomcat']['catalina_base_dir']}/Catalina/localhost/host-manager.xml" do
+    action :delete
+  end
+end
+
 tomcat_pkgs.each do |pkg|
   package pkg do
     action :install
